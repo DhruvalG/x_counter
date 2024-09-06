05ffb356e4ab2688026758bd5d54e4991a955e6a
DhruvalG <dhruval.gv@gmail.com>
2024-09-06 22:07:57 +0530

    fixed counter increment and decrement

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,8 +7,8 @@ let Counter = () => {
     <div>
       <h1>Counter App</h1>
       <p>Count: {counter}</p>
-      <button onClick={() => setCounter(counter + 3)}>Increment</button>
-      <button onClick={() => setCounter(counter - 2)}>Decrement</button>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
     </div>
   );
 };
